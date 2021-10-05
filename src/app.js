756031eef4df0ea3b71f2fc2d5078a9be8c5dd3f
shravan20 <mrshravankumarb@gmail.com>
2021-10-05 10:32:18 +0530

    [ Google SSO ] | refactor | Google SSO Setup  with Passport

--- src/app.js
+++ src/app.js
@@ -43,7 +43,6 @@ passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback"
-    // callbackURL: "http://localhost:3000/success"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
