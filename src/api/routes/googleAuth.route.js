9fa55eb6da497943f17601daca9faa48ebc523d1
shravan20 <mrshravankumarb@gmail.com>
2021-10-11 23:53:19 +0530

    [ Route ]| Fix | Fixed route

---
+++ src/api/routes/googleAuth.route.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require('passport');
+const authController = require('../controllers/auth.controller');
+
+
+router.get('/',
+    passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+router.get('/callback',
+    passport.authenticate('google', { failureRedirect: '/error' }),
+);
+
+
+
+module.exports = router;
